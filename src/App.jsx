ce2d849c17f60849d5db0d5d34db214ac4deaceb
Muhammed <faraanshaik19@gmail.com>
2025-04-29 00:51:14 +0530

    Navbar component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 const tempMovieData = [
   {
@@ -63,23 +64,7 @@ export default function App() {
 
   return (
     <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🎬 </span>
-          <h1>Movie Verdict</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
-
+      <Navbar query={query} setQuery={setQuery} movies={movies} />
       <main className="main">
         <div className="box">
           <button
