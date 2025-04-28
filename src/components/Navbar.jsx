eb9f4229053ed23817a856d12d725b29738c88c6
Muhammed <faraanshaik19@gmail.com>
2025-04-29 01:19:34 +0530

    new components commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Logo from "./Logo";
 import SearchBox from "./SearchBox";
+import NumResults from "./NumResults";
 
 const Navbar = ({ query, setQuery, movies }) => {
   return (
     <nav className="nav-bar">
       <Logo />
       <SearchBox query={query} setQuery={setQuery} />
-      <p className="num-results">
-        Found <strong>{movies.length}</strong> results
-      </p>
+      <NumResults movies={movies} />
     </nav>
   );
 };
---
+++ src/components/WatchedBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WatchedBox = () => {
+  return <div>WatchedBox</div>;
+};
+
+export default WatchedBox;
