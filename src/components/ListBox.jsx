2087f477c35ac9d0a42c8a6dd30f40fb969f7533
Muhammed <faraanshaik19@gmail.com>
2025-04-29 01:43:35 +0530

    MoviesList and WatchedList commit

--- src/components/ListBox.jsx
+++ src/components/ListBox.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import MoviesList from "./MoviesList";
 
-const ListBox = () => {
-  return <div>ListBox</div>;
+const ListBox = ({ movies }) => {
+  const [isOpen1, setIsOpen1] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && <MoviesList movies={movies} />}
+    </div>
+  );
 };
 
 export default ListBox;
